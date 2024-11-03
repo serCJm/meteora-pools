98767533aafde4834074c1488c9eaf2bded5c2e3
CJ Moro <25824081+serCJm@users.noreply.github.com>
2024-11-03 00:57:42 +0000

    Add error handling

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-import { startBot } from "./src/bot.js";
+import { startBot } from "./src/bot/bot.js";
 
 /**
  * Starts the Telegram bot.
  */
 function start() {
-	const token = process.env.BOT_TOKEN;
-	if (!token) {
-		console.error("Error: BOT_TOKEN environment variable not set.");
-		return;
+	try {
+		const token = process.env.BOT_TOKEN;
+		if (!token) {
+			console.error("Error: BOT_TOKEN environment variable not set.");
+			return;
+		}
+		startBot(token);
+	} catch (error) {
+		console.error(error);
 	}
-	startBot(token);
 }
 
 start();
