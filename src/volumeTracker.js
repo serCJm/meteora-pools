781e4bd5d5f173140e5587d52960fcb42058582a
CJ Moro <25824081+serCJm@users.noreply.github.com>
2024-11-05 23:52:37 +0000

    Add notify new pools

--- src/volumeTracker.js
+++ src/volumeTracker.js
@@ -1,9 +1,13 @@
 import { getDLMMPools } from "./api/meteora.js";
+import { bot } from "./api/telegram.js";
 import PoolVolume from "./models/poolsVolume.js";
+import Subscription from "./models/subscription.js";
 
 export async function fetchVolumeDataAndUpdate() {
 	try {
 		const dexDataList = await getDLMMPools();
+
+		await notifyNewPools(dexDataList);
 
 		const bulkOps = dexDataList.map((dexData) => {
 			const poolAddress = dexData.address;
@@ -29,4 +33,50 @@ export async function fetchVolumeDataAndUpdate() {
 	} catch (err) {
 		console.error("Error fetching DEX data:", err);
 	}
+}
+
+/**
+ * Identifies new pool addresses by comparing a list of API pool addresses
+ * against existing pool addresses in the database.
+ *
+ * @async
+ * @function findNewPoolAddresses
+ * @param {Array<Object>} dexDataList - An array of API pool objects.
+ * @returns {Promise<Array<string>>} A promise that resolves to an array of new pool addresses.
+ */
+async function notifyNewPools(dexDataList) {
+	const apiPoolAddresses = dexDataList.map((dexData) => dexData.address);
+
+	const existingPools = await PoolVolume.find(
+		{ poolAddress: { $in: apiPoolAddresses } },
+		{ poolAddress: 1 }
+	);
+	const existingPoolAddresses = existingPools.map((pool) => pool.poolAddress);
+
+	const newPoolAddresses = apiPoolAddresses.filter(
+		(address) => !existingPoolAddresses.includes(address)
+	);
+
+	if (newPoolAddresses.length > 0) {
+		console.log(
+			`Found ${newPoolAddresses.length} new pool(s):`,
+			newPoolAddresses
+		);
+		const subscriptions = await Subscription.find({ newPools: true });
+		const messages = subscriptions.map((subscription) => {
+			return bot.telegram.sendMessage(
+				subscription.chatId,
+				newPoolAddresses.join("\n"),
+				{
+					parse_mode: "HTML",
+					// @ts-ignore
+					disable_web_page_preview: true
+				}
+			);
+		});
+
+		await Promise.all(messages);
+	}
+
+	return newPoolAddresses;
 }
