cb555b1b77ae9b00f0c6b3dcbd01e05691f54adc
CJ Moro <25824081+serCJm@users.noreply.github.com>
2024-11-06 23:51:25 +0000

    Add increasing volume tracker

--- src/volumeTracker.js
+++ src/volumeTracker.js
@@ -20,7 +20,14 @@ export async function fetchVolumeDataAndUpdate() {
 			return {
 				updateOne: {
 					filter: { poolAddress },
-					update: { $push: { volumes: volumeEntry } },
+					update: {
+						$push: {
+							volumes: {
+								$each: [volumeEntry],
+								$slice: -10
+							}
+						}
+					},
 					upsert: true
 				}
 			};
@@ -30,6 +37,8 @@ export async function fetchVolumeDataAndUpdate() {
 		console.log(
 			"DEX data fetched and updated successfully using bulk operations."
 		);
+
+		await notifyIncreasingVolumePools(dexDataList);
 	} catch (err) {
 		console.error("Error fetching DEX data:", err);
 	}
@@ -44,30 +53,118 @@ export async function fetchVolumeDataAndUpdate() {
  * @param {Array<Object>} dexDataList - An array of API pool objects.
  */
 async function notifyNewPools(dexDataList) {
-	const apiPoolAddresses = dexDataList.map((dexData) => dexData.address);
+	try {
+		const apiPoolAddresses = dexDataList.map((dexData) => dexData.address);
 
-	const existingPools = await PoolVolume.find(
-		{ poolAddress: { $in: apiPoolAddresses } },
-		{ poolAddress: 1 }
-	);
-	const existingPoolAddresses = existingPools.map((pool) => pool.poolAddress);
+		const existingPools = await PoolVolume.find(
+			{ poolAddress: { $in: apiPoolAddresses } },
+			{ poolAddress: 1 }
+		);
+		const existingPoolAddresses = existingPools.map(
+			(pool) => pool.poolAddress
+		);
 
-	const newPools = dexDataList.filter(
-		(pool) => !existingPoolAddresses.includes(pool.address)
-	);
+		const newPools = dexDataList.filter(
+			(pool) => !existingPoolAddresses.includes(pool.address)
+		);
 
-	if (newPools.length > 0 && existingPoolAddresses.length > 0) {
-		const subscriptions = await Subscription.find({ newPools: true });
-		const messages = subscriptions.map((subscription) => {
-			return sendChunkedPoolInformation(
-				subscription.chatId,
-				newPools,
-				[{ field: "liquidity", order: "desc" }],
-				newPools.length,
-				`New pools found: ${newPools.length}`
+		if (newPools.length > 0 && existingPoolAddresses.length > 0) {
+			const subscriptions = await Subscription.find({ newPools: true });
+			const messages = subscriptions.map((subscription) => {
+				return sendChunkedPoolInformation(
+					subscription.chatId,
+					newPools,
+					[{ field: "liquidity", order: "desc" }],
+					newPools.length,
+					`New pools found: ${newPools.length}`
+				);
+			});
+
+			await Promise.all(messages);
+			console.log("Successfully notified new pools.");
+		} else {
+			console.log("No new pools found.");
+		}
+	} catch (error) {
+		console.error("Error notifying increasing volume pools:", error);
+	}
+}
+
+async function notifyIncreasingVolumePools(dexDataList) {
+	try {
+		const increasingVolumePools = await PoolVolume.aggregate([
+			{ $match: { "volumes.4": { $exists: true } } },
+			{
+				$project: {
+					poolAddress: 1,
+					volumes: { $slice: ["$volumes", -5] }
+				}
+			},
+			{
+				$addFields: {
+					isIncreasing: {
+						$and: [
+							{
+								$lt: [
+									{ $arrayElemAt: ["$volumes.volume", 0] },
+									{ $arrayElemAt: ["$volumes.volume", 1] }
+								]
+							},
+							{
+								$lt: [
+									{ $arrayElemAt: ["$volumes.volume", 1] },
+									{ $arrayElemAt: ["$volumes.volume", 2] }
+								]
+							},
+							{
+								$lt: [
+									{ $arrayElemAt: ["$volumes.volume", 2] },
+									{ $arrayElemAt: ["$volumes.volume", 3] }
+								]
+							},
+							{
+								$lt: [
+									{ $arrayElemAt: ["$volumes.volume", 3] },
+									{ $arrayElemAt: ["$volumes.volume", 4] }
+								]
+							}
+						]
+					}
+				}
+			},
+			{ $match: { isIncreasing: true } }
+		]);
+
+		if (increasingVolumePools.length > 0) {
+			const increasingVolumePoolAddresses = increasingVolumePools.map(
+				(pool) => pool.poolAddress
 			);
-		});
+			const filteredPools = dexDataList.filter((pool) =>
+				increasingVolumePoolAddresses.includes(pool.address)
+			);
 
-		await Promise.all(messages);
+			if (filteredPools.length > 0) {
+				const subscriptions = await Subscription.find({
+					increasedVolume: true
+				});
+				const messages = subscriptions.map((subscription) => {
+					return sendChunkedPoolInformation(
+						subscription.chatId,
+						filteredPools,
+						[{ field: "liquidity", order: "desc" }],
+						filteredPools.length,
+						`Pools with increasing volume over the last 5 hours: ${filteredPools.length}`
+					);
+				});
+				await Promise.all(messages);
+				console.log("Successfully notified increasing volume pools.");
+			} else {
+				console.log(
+					"No pools with increasing volume over the last 5 hours."
+				);
+			}
+		}
+	} catch (error) {
+		console.error("Error notifying increasing volume pools:", error);
 	}
 }
