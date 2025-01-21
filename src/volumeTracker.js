13ecd3fb16ae69145c6ff2ebe89443efde43c167
CJ Moro <25824081+serCJm@users.noreply.github.com>
2025-01-21 22:59:02 +0000

    Add logs

--- src/volumeTracker.js
+++ src/volumeTracker.js
@@ -5,6 +5,7 @@ import Subscription from "./models/subscription.js";
 
 export async function fetchVolumeDataAndUpdate() {
 	try {
+		consloe.log("Fetching DEX data...");
 		const dexDataList = await getDLMMPools();
 
 		await notifyNewPools(dexDataList);
